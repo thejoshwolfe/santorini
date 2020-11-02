ec75189d34f1f22780e74846dfd2ed65438e2dd9
Winnie Chen <mail@winniech.com>
2020-11-01 23:08:39 -0500

    change cubes to look a little more santorini

--- public/js/main.js
+++ public/js/main.js
@@ -6,15 +6,15 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 // cubes
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-cube.position.set( 0, 0, 0 );
+const midCube = new THREE.BoxGeometry(.9, .9, .9);
+const material = new THREE.MeshLambertMaterial();
+const cube = new THREE.Mesh( midCube, material );
+cube.position.set( 0, -1, 0 );
 
-const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const cube2 = new THREE.Mesh( geometry, material2 );
-cube2.position.set( 0, 1, 0 );
-
+const baseCube = new THREE.BoxGeometry();
+const material2 = new THREE.MeshLambertMaterial();
+const cube2 = new THREE.Mesh( baseCube, material2 );
+cube2.position.set( 0, -2, 0 );
 
 // Group cubes into stack
 const cubeStack = new THREE.Group();
