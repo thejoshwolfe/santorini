798f991697dbf9af2b7442e2ddffdc0c57e1c6e8
Winnie Chen <19295645+win-chen@users.noreply.github.com>
2020-11-14 13:18:03 -0500

    move lighting into separate file

--- public/js/main.js
+++ public/js/main.js
@@ -77,24 +77,7 @@ function createPawn(x, y) {
 	createPawn(x, y);
 });
 
-// Add lighting
-const skyColor = 0xB1E1FF;  // light blue
-const groundColor = 0xB97A20;  // brownish orange
-const intensity = 1;
-const ambientLight = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-scene.add(ambientLight);
-
-const light = new THREE.DirectionalLight(0xFFFFFF, 0.3);
-light.position.set(5, 20, 10);
-light.target.position.set(0, 0, 0);
-scene.add(light);
-scene.add(light.target);
-
-const secondLight = new THREE.DirectionalLight(0xFFFFFF, 0.3);
-secondLight.position.set(-10, 20, -5);
-secondLight.target.position.set(0, 0, 0);
-scene.add(secondLight);
-scene.add(secondLight.target);
+addLighting(scene);
 
 // camera
 let cameraAngleY = 0;
